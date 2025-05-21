58c1cea32a3d948a45bfcadb525a800ab6aa8e81
KwonYunje <yunje0012@gmail.com>
2025-05-21 13:05:46 +0900

    +=

---
+++ Swea21425PlusEqual.java
@@ -0,0 +1,39 @@
+package VsCode_Java.Java_Problem;
+
+import java.util.Scanner;
+
+public class Swea21425PlusEqual {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int t = sc.nextInt();
+            sc.nextLine();
+
+            int[] answer = new int[t];
+
+            for(int tc = 0 ; tc < t ; tc++){
+                String[] line = sc.nextLine().split(" ");
+
+                int x = Integer.parseInt( line[0] );
+                int y = Integer.parseInt( line[1] );
+                int min = Integer.parseInt( line[2] );
+
+                int count = 0 ;
+
+                while (x <= min && y <= min) {
+                    if(x >= y) {
+                        y += x;
+                    }else{
+                        x += y;
+                    }
+                    count++;
+                }
+
+                answer[tc] = count;
+            }
+            
+            for(int i = 0 ; i < answer.length ; i++){
+                System.out.println(answer[i]);
+            }
+        }
+    }
