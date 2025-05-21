05ea8c992ec81e36594ede04a2619614ad2ae664
KwonYunje <yunje0012@gmail.com>
2025-05-21 18:19:49 +0900

    문자열 옮기기(시간복잡도 포함)

--- Swea22979MoveString.java
+++ Swea22979MoveString.java
@@ -24,8 +24,11 @@ public class Swea22979MoveString {
             for(int i = 0 ; i < cycleArr.length ; i++){
                 int move = cycleArr[i] % str.length(); // 길이 넘어가면 의미 없음(중요!! : 무의미한 반복문으로 인해 시간복잡도가 상승함!!)
 
+                //매번 반복하지 말고 위에서 길이 이내로 자른 반복횟수만큼의 길이를 뒤에서부터 잘라 앞으로 붙인다.
                 if (move > 0) { // 왼쪽으로 move번 이동
                     str = str.substring(move) + str.substring(0, move);
+                
+                //동일하게 매번 반복하지 말고 길이 이내로 자른 반복횟수만큼 길이를 앞에서 잘라 뒤에 붙인다.
                 } else if (move < 0) { // 오른쪽으로 -move번 이동
                     move = -move;
                     str = str.substring(str.length() - move) + str.substring(0, str.length() - move);
