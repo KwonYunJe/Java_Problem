bbb311533d4575097d5cb1b12e1e6a08a189e9d4
yunje0012@gmail.com <yunje0012@gmai.com>
2025-07-23 11:10:28 +0900

    백준 10989.수 정렬하기 3

---
+++ Backjoon10989SortingNumber.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Backjoon10989SortingNumber {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for(int i = 0 ; i < arr.length ; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for(int i = 0 ; i < arr.length ; i++){
+            bw.write(String.valueOf(arr[i])+"\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
